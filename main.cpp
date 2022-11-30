7642596fd4708e1fdeb8c2df310a0c345c2b3ae1
Mert Hidayetoglu <merth@stanford.edu>
2022-11-30 12:24:42 -0800

    templetize patch size

--- main.cpp
+++ main.cpp
@@ -26,37 +26,41 @@ private:
   const unsigned numlevel;
   Patch<D, S, T> *patchlist;
   
-
 public:
   Grid(const unsigned dim[], const unsigned numlevel) : numlevel(numlevel) {
+
     for(auto d = 0; d < D; d++) {
       this->dim[d] = dim[d];
       numpatch *= dim[d];
     }
+    patchlist = new Patch<D, S, T>[numpatch];
 
     // REPORT BASE GRID
-    printf("\nA %d-dimensional ( ", D);
-    for(auto d = 0; d < D; d++)
-      printf("%d ", this->dim[d]);
-    printf(") grid of %d patches with size %d is created.\n", numpatch, S);
-    printf("The base grid is ");
-    for(auto d = 0; d < D; d++)
-      printf("%d ", this->dim[d] * S);
-    printf(".\n\n");
-
-    patchlist = new Patch<D, S, T>[numpatch];
+    {
+      printf("\nA %d-dimensional ( ", D);
+      for(auto d = 0; d < D; d++)
+        printf("%d ", this->dim[d]);
+      printf(") grid of %d patches with size %d is created.\n", numpatch, S);
+      unsigned long points = 1;
+      printf("The base has ");
+      for(auto d = 0; d < D; d++) {
+        printf("%d ", this->dim[d] * S);
+        points *= this->dim[d] * S;
+      }
+      printf(" = %ld data points.\n\n", points);
+    }
   };
 };
 
 #define DTYPE double // FIELD DATA TYPE
-#define NDIM 2 // NUMBER OF DIMENSIONS
+#define NDIM 3 // NUMBER OF DIMENSIONS
 #define PSIZE 32 // PATCH SIZE
 
 int main(int argc, char *argv[]) {
 
-  auto numlevel = 5;
+  auto numlevel = 4;
 
-  unsigned int dim[NDIM] = {5, 6};
+  const unsigned dim[NDIM] = {5, 6, 7};
 
   Grid<NDIM, PSIZE, DTYPE> base(dim, numlevel);
 
