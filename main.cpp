fec811c22e3a8c86cce9dbd220e9587f1e8f005e
Mert Hidayetoglu <merth@stanford.edu>
2022-11-30 12:19:56 -0800

    patch structure is created

--- main.cpp
+++ main.cpp
@@ -1,53 +1,63 @@
 #include <stdio.h>
-#include "amrh.h"
-
-#define DTYPE double // FIELD DATA TYPE
-#define NDIM 3 // NUMBER OF DIMENSIONS
-#define NLEVEL 7 // NUMBER OF LEVELS
-#define PSIZE 32 // PATCH SIZE
 
 // COMPILE-TIME POWER OPERATION
-constexpr unsigned int ipow(unsigned int num, unsigned int pow)
+constexpr unsigned ipow(unsigned num, unsigned pow)
 {
-    return (pow >= sizeof(unsigned int)*8) ? 0 :
-        pow == 0 ? 1 : num * ipow(num, pow-1);
+    return (pow >= sizeof(unsigned ) * 8) ? 0 :
+        pow == 0 ? 1 : num * ipow(num, pow - 1);
 }
 
-template <unsigned int D, typename T>
+template <unsigned D, unsigned S, typename T>
 struct Patch {
 
-  T *data = new T[ipow(PSIZE, D)];
+  const unsigned ndata = ipow(S, D);
+
+  T const *data = new T[ndata];
+
 
 };
 
-template <unsigned int D, typename T>
-struct Base {
+template <unsigned D, unsigned S, typename T>
+struct Grid {
 
 private:
-  unsigned int dim[D];
-  unsigned int nbasepatch = 1;
-  Patch<D, T> *patchlist;
+  unsigned dim[D];
+  unsigned numpatch = 1;
+  const unsigned numlevel;
+  Patch<D, S, T> *patchlist;
   
 
 public:
-  Base(unsigned int dim[]) {
+  Grid(const unsigned dim[], const unsigned numlevel) : numlevel(numlevel) {
     for(auto d = 0; d < D; d++) {
       this->dim[d] = dim[d];
-      nbasepatch *= dim[d];
+      numpatch *= dim[d];
     }
-    printf("A Base with dims ");
-    for(int d = 0; d < D; d++)
-      printf("%d ", this->dim[d]);
-    printf("is created with %d base patches.\n", nbasepatch);
 
-    patchlist = new Patch<D,T>[nbasepatch];
+    // REPORT BASE GRID
+    printf("\nA %d-dimensional ( ", D);
+    for(auto d = 0; d < D; d++)
+      printf("%d ", this->dim[d]);
+    printf(") grid of %d patches with size %d is created.\n", numpatch, S);
+    printf("The base grid is ");
+    for(auto d = 0; d < D; d++)
+      printf("%d ", this->dim[d] * S);
+    printf(".\n\n");
+
+    patchlist = new Patch<D, S, T>[numpatch];
   };
 };
 
+#define DTYPE double // FIELD DATA TYPE
+#define NDIM 2 // NUMBER OF DIMENSIONS
+#define PSIZE 32 // PATCH SIZE
 
 int main(int argc, char *argv[]) {
 
-  unsigned int dim[NDIM] = {12, 13, 14};
-  Base<NDIM, DTYPE> base(dim);
+  auto numlevel = 5;
+
+  unsigned int dim[NDIM] = {5, 6};
+
+  Grid<NDIM, PSIZE, DTYPE> base(dim, numlevel);
 
 }
