d3cae402833f0ee4a18ae222bdc22fdeee637d76
Hang Song <songhang@stanford.edu>
2025-04-04 11:06:31 -0700

    Finished Riemann solver

--- tests/Implosion.py
+++ tests/Implosion.py
@@ -6,17 +6,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import h5py
+from numba import jit, float64, int16, int32
+from numba.experimental import jitclass
 
 dt = 0.001
 nsteps = 2000
 fig_output_stride = 100
-fig_name_fmt = "./VortexFigs/vis_{:06d}.pdf"
-dat_name_fmt = "./VortexData/vort_{:06d}.h5"
+fig_name_fmt = "./ImplosionFigs/vis_implosion_{:06d}.pdf"
+dat_name_fmt = "./ImplosionData/dat_implosion_{:06d}.h5"
 
 Rg    = 1.0
 gamma = 1.4
 L     = 12.0
-N     = 512
+N     = 128
 
 def main():
     cvars_0 = ConsVars(N)
@@ -34,7 +36,7 @@ def main():
     visualizeSolution(cvars_0.mass, fig_name_fmt.format(fig_count))
     saveSolution(cvars_0.convertToPrim(), ['rho', 'u', 'v', 'T', 'p'], dat_name_fmt.format(fig_count))
     for tid in range(nsteps):
-        SSPRK3(cvars_0, cvars_1, cvars_2, dx, dy, dt)
+        SSPRK3Riemann(cvars_0, cvars_1, cvars_2, dx, dy, dt)
         if (((tid + 1) % fig_output_stride) == 0):
             fig_count += 1
             visualizeSolution(cvars_0.mass, fig_name_fmt.format(fig_count))
@@ -65,11 +67,24 @@ class ConsVars:
         self.mmty = np.empty((N, N), dtype=np.dtype(np.float64, align=True))
         self.enrg = np.empty((N, N), dtype=np.dtype(np.float64, align=True))
 
+    def get(self):
+        return [self.mass, self.mmtx, self.mmty, self.enrg]
+
     def convertToPrim(self):
         rho = self.mass
         u   = self.mmtx / self.mass
         v   = self.mmty / self.mass
         T   = (self.enrg / self.mass - 0.5 * (u * u + v * v)) * (gamma - 1.0) / Rg
+        p   = rho * Rg * T
+        return rho, u, v, T, p
+
+
+@jit(nopython=True)
+def conservativeToPrimitive(mass, mmtx, mmty, enrg, gamma, Rg):
+        rho = mass
+        u   = mmtx / mass
+        v   = mmty / mass
+        T   = (enrg / mass - 0.5 * (u * u + v * v)) * (gamma - 1.0) / Rg
         p   = rho * Rg * T
         return rho, u, v, T, p
 
@@ -95,9 +110,70 @@ def setInitialCondition(cvars : ConsVars, x, y):
     cvars.mmty[:] = rho * v
     cvars.enrg[:] = rho * (Rg * T / (gamma - 1.0) + 0.5 * (u * u + v * v))
 
+
+@jit(nopython=True)
+def roeAvg(rhoL, rhoR, uL, uR, vL, vR, TL, TR):
+    rhoL_sqrt = rhoL ** 0.5
+    rhoR_sqrt = rhoR ** 0.5
+    rho_fact  = 1.0 / (rhoL_sqrt + rhoR_sqrt)
+
+    cv = Rg / (gamma - 1.0)
+    qL = uL * uL + vL * vL
+    qR = uR * uR + vR * vR
+    eL = cv * TL + 0.5 * qL 
+    eR = cv * TR + 0.5 * qR 
+    hL = eL + Rg * TL 
+    hR = eR + Rg * TR
+
+    rho_avg   = rhoL_sqrt * rhoR_sqrt
+    u_avg     = (rhoL_sqrt * uL + rhoR_sqrt * uR) * rho_fact
+    v_avg     = (rhoL_sqrt * vL + rhoR_sqrt * vR) * rho_fact
+    e_avg     = (rhoL_sqrt * eL + rhoR_sqrt * eR) * rho_fact
+    h_avg     = (rhoL_sqrt * hL + rhoR_sqrt * hR) * rho_fact
+    c_avg     = ((gamma - 1.0) * (h_avg - 0.5 * (u_avg * u_avg + v_avg * v_avg))) ** 0.5
+
+    return u_avg, v_avg, c_avg, h_avg
+
+@jit(nopython=True)
+def inviscidFluxes(U, g):
+    '''
+    Assemble Euler fluxe
+    Params:
+        U - vector of conservative variables
+        g - unit normal vectors
+    '''
+    F = np.empty_like(U)
+    u = U[1] / U[0]
+    v = U[2] / U[0]
+    p = (U[3] - 0.5 * U[0] * (u * u + v * v)) * (gamma - 1.0)
+    u_convec = u * g[0] + v * g[1]
+    F[0] = U[0] * u_convec
+    F[1] = U[1] * u_convec + p * g[0]
+    F[2] = U[2] * u_convec + p * g[1]
+    F[3] = (U[3] + p) * u_convec
+    return F
+    
+
+@jit(nopython=True)
+def riemannFluxes(UL, UR, S, g):
+    FL = inviscidFluxes(UL, g)
+    FR = inviscidFluxes(UR, g)
+    F = 0.5 * (FR + FL) - 0.5 * S * (UR - UL)
+    return F
+
+
+@jit(nopython=True)
 def characteristicToConservative(ch0, ch1, ch2, ch3, c_avg, u_avg, v_avg, h_avg, g, l):
     '''
     Convert characteristic variables to conservative variables
+    Params:
+    ch0, ch1, ch2, ch3 -- characteristic variables in 2D
+    U0, U1, U2, U3     -- conservative variables in 2D
+    c_avg              -- Roe-Pike-averaged speed of sound
+    u_avg              -- Roe-Pike-averaged x-velocity
+    v_avg              -- Roe-Pike-averaged y-velocity
+    g                  -- 2D unit normal vector
+    l                  -- 2D unit tangential vector
     '''
     Ug = u_avg * g[0] + v_avg * g[1]
     Ul = u_avg * l[0] + v_avg * l[1]
@@ -108,17 +184,25 @@ def characteristicToConservative(ch0, ch1, ch2, ch3, c_avg, u_avg, v_avg, h_avg,
     U3 = ch0 * (h_avg - c_avg * Ug) + ch1 * Ul + ch2 * ek_avg + ch3 * (h_avg + c_avg * Ug)
     return U0, U1, U2, U3
 
-    
+
+@jit(nopython=True)
 def conservativeToCharacteristic(U0, U1, U2, U3, c_avg, u_avg, v_avg, g, l):
     '''
     Convert conservative variables to characteristic variables
+    Params:
+    ch0, ch1, ch2, ch3 -- characteristic variables in 2D
+    U0, U1, U2, U3     -- conservative variables in 2D
+    c_avg              -- Roe-Pike-averaged speed of sound
+    u_avg              -- Roe-Pike-averaged x-velocity
+    v_avg              -- Roe-Pike-averaged y-velocity
+    g                  -- 2D unit normal vector
+    l                  -- 2D unit tangential vector
     '''
     rho = U0
     u   = U1 / U0
     v   = U2 / U0
     T   = (U3 / U0 - 0.5 * (u * u + v * v)) * (gamma - 1.0) / Rg
-    c   = np.sqrt(gamma * Rg * T)
-
+    c   = (gamma * Rg * T) ** 0.5
 
     ek_avg = 0.5 * (u_avg * u_avg + v_avg * v_avg)
     gm1byc2 = 0.5 * (gamma - 1.0) / (c_avg * c_avg)
@@ -144,123 +228,160 @@ def conservativeToCharacteristic(U0, U1, U2, U3, c_avg, u_avg, v_avg, g, l):
     
     return ch0, ch1, ch2, ch3
 
-def SSPRK3(cvars_0 : ConsVars, cvars_1 : ConsVars, cvars_2 : ConsVars, dx : float, dy : float, dt : float):
-    calcRHS(cvars_1, cvars_0, dx, dy)
+
+
+@jit(nopython=True, parallel=True)
+def calcRHSRiemann(cvars_ddt, cvars_now, dx : float, dy : float):
+    '''
+    Calculate RHS leaf task
+    '''
+    U0 = cvars_now[0]
+    U1 = cvars_now[1]
+    U2 = cvars_now[2]
+    U3 = cvars_now[3]
+    F0 = np.empty_like(U0)
+    F1 = np.empty_like(U1)
+    F2 = np.empty_like(U2)
+    F3 = np.empty_like(U3)
+
+    rho, u, v, T, p = conservativeToPrimitive(U0, U1, U2, U3, gamma, Rg)
+
+    # Compute x-flux: The indices of edges align with the lower sides of the nodes
+    for i in range(N):
+        for j in range(N):
+            u_avg, v_avg, c_avg, h_avg = roeAvg(rho[i-1, j], rho[i, j], \
+                                                u  [i-1, j], u  [i, j], \
+                                                v  [i-1, j], v  [i, j], \
+                                                T  [i-1, j], T  [i, j])
+            ch0 = np.empty((6,), dtype=np.float64)
+            ch1 = np.empty((6,), dtype=np.float64)
+            ch2 = np.empty((6,), dtype=np.float64)
+            ch3 = np.empty((6,), dtype=np.float64)
+
+            for m in range(6):
+                im = i - 3 + m
+                ch0[m], ch1[m], ch2[m], ch3[m] = conservativeToCharacteristic( \
+                        U0[im, j], U1[im, j], U2[im, j], U3[im, j], \
+                        c_avg, u_avg, v_avg, [1.0, 0.0], [0.0, 1.0])
+
+            UL = np.empty((4,), dtype=np.float64)
+            UR = np.empty((4,), dtype=np.float64)
+
+            ch0I = weno5ZInterp(ch0[0], ch0[1], ch0[2], ch0[3], ch0[4])
+            ch1I = weno5ZInterp(ch1[0], ch1[1], ch1[2], ch1[3], ch1[4])
+            ch2I = weno5ZInterp(ch2[0], ch2[1], ch2[2], ch2[3], ch2[4])
+            ch3I = weno5ZInterp(ch3[0], ch3[1], ch3[2], ch3[3], ch3[4])
+            UL[0], UL[1], UL[2], UL[3] = characteristicToConservative( \
+                    ch0I, ch1I, ch2I, ch3I, c_avg, u_avg, v_avg, h_avg, [1.0, 0.0], [0.0, 1.0])
+
+            ch0I = weno5ZInterp(ch0[5], ch0[4], ch0[3], ch0[2], ch0[1])
+            ch1I = weno5ZInterp(ch1[5], ch1[4], ch1[3], ch1[2], ch1[1])
+            ch2I = weno5ZInterp(ch2[5], ch2[4], ch2[3], ch2[2], ch2[1])
+            ch3I = weno5ZInterp(ch3[5], ch3[4], ch3[3], ch3[2], ch3[1])
+            UR[0], UR[1], UR[2], UR[3] = characteristicToConservative( \
+                    ch0I, ch1I, ch2I, ch3I, c_avg, u_avg, v_avg, h_avg, [1.0, 0.0], [0.0, 1.0])
+
+            S = np.float64(max(abs(u[i-1, j]), abs(u[i, j]))) + c_avg
+            F0[i,j], F1[i,j], F2[i,j], F3[i,j] = riemannFluxes(UL, UR, S, [1.0, 0.0])
+
+    cvars_ddt[0][:] = ddxStag(F0, -1.0/dx)
+    cvars_ddt[1][:] = ddxStag(F1, -1.0/dx)
+    cvars_ddt[2][:] = ddxStag(F2, -1.0/dx)
+    cvars_ddt[3][:] = ddxStag(F3, -1.0/dx)
+            
+    # Compute y-flux: The indices of edges align with the lower sides of the nodes
+    for i in range(N):
+        for j in range(N):
+            u_avg, v_avg, c_avg, h_avg = roeAvg(rho[i, j-1], rho[i, j], \
+                                                u  [i, j-1], u  [i, j], \
+                                                v  [i, j-1], v  [i, j], \
+                                                T  [i, j-1], T  [i, j])
+            ch0 = np.empty((6,), dtype=np.float64)
+            ch1 = np.empty((6,), dtype=np.float64)
+            ch2 = np.empty((6,), dtype=np.float64)
+            ch3 = np.empty((6,), dtype=np.float64)
+
+            for m in range(6):
+                jm = j - 3 + m
+                ch0[m], ch1[m], ch2[m], ch3[m] = conservativeToCharacteristic( \
+                        U0[i, jm], U1[i, jm], U2[i, jm], U3[i, jm], \
+                        c_avg, u_avg, v_avg, [0.0, 1.0], [1.0, 0.0])
+
+            UL = np.empty((4,), dtype=np.float64)
+            UR = np.empty((4,), dtype=np.float64)
+
+            ch0I = weno5ZInterp(ch0[0], ch0[1], ch0[2], ch0[3], ch0[4])
+            ch1I = weno5ZInterp(ch1[0], ch1[1], ch1[2], ch1[3], ch1[4])
+            ch2I = weno5ZInterp(ch2[0], ch2[1], ch2[2], ch2[3], ch2[4])
+            ch3I = weno5ZInterp(ch3[0], ch3[1], ch3[2], ch3[3], ch3[4])
+            UL[0], UL[1], UL[2], UL[3] = characteristicToConservative( \
+                    ch0I, ch1I, ch2I, ch3I, c_avg, u_avg, v_avg, h_avg, [0.0, 1.0], [1.0, 0.0])
+
+            ch0I = weno5ZInterp(ch0[5], ch0[4], ch0[3], ch0[2], ch0[1])
+            ch1I = weno5ZInterp(ch1[5], ch1[4], ch1[3], ch1[2], ch1[1])
+            ch2I = weno5ZInterp(ch2[5], ch2[4], ch2[3], ch2[2], ch2[1])
+            ch3I = weno5ZInterp(ch3[5], ch3[4], ch3[3], ch3[2], ch3[1])
+            UR[0], UR[1], UR[2], UR[3] = characteristicToConservative( \
+                    ch0I, ch1I, ch2I, ch3I, c_avg, u_avg, v_avg, h_avg, [0.0, 1.0], [1.0, 0.0])
+
+            S = np.float64(max(abs(u[i, j-1]), abs(u[i, j]))) + c_avg
+            F0[i,j], F1[i,j], F2[i,j], F3[i,j] = riemannFluxes(UL, UR, S, [0.0, 1.0])
+
+    cvars_ddt[0][:] += ddyStag(F0, -1.0/dy)
+    cvars_ddt[1][:] += ddyStag(F1, -1.0/dy)
+    cvars_ddt[2][:] += ddyStag(F2, -1.0/dy)
+    cvars_ddt[3][:] += ddyStag(F3, -1.0/dy)
+            
+
+
+
+def SSPRK3Riemann(cvars_0 : ConsVars, cvars_1 : ConsVars, cvars_2 : ConsVars, dx : float, dy : float, dt : float):
+    calcRHSRiemann(cvars_1.get(), cvars_0.get(), dx, dy)
     cvars_1.mass[:] = cvars_0.mass + (dt * cvars_1.mass)
     cvars_1.mmtx[:] = cvars_0.mmtx + (dt * cvars_1.mmtx)
     cvars_1.mmty[:] = cvars_0.mmty + (dt * cvars_1.mmty)
     cvars_1.enrg[:] = cvars_0.enrg + (dt * cvars_1.enrg)
 
-    calcRHS(cvars_2, cvars_1, dx, dy)
+    calcRHSRiemann(cvars_2.get(), cvars_1.get(), dx, dy)
     cvars_2.mass[:] = (0.75 * cvars_0.mass) + (0.25 * cvars_1.mass) + (0.25 * dt * cvars_1.mass)
     cvars_2.mmtx[:] = (0.75 * cvars_0.mmtx) + (0.25 * cvars_1.mmtx) + (0.25 * dt * cvars_1.mmtx)
     cvars_2.mmty[:] = (0.75 * cvars_0.mmty) + (0.25 * cvars_1.mmty) + (0.25 * dt * cvars_1.mmty)
     cvars_2.enrg[:] = (0.75 * cvars_0.enrg) + (0.25 * cvars_1.enrg) + (0.25 * dt * cvars_1.enrg)
 
-    calcRHS(cvars_1, cvars_2, dx, dy)
+    calcRHSRiemann(cvars_1.get(), cvars_2.get(), dx, dy)
     cvars_0.mass[:] = ((1.0/3.0) * cvars_0.mass) + ((2.0/3.0) * cvars_2.mass) + ((2.0/3.0) * dt * cvars_1.mass)
     cvars_0.mmtx[:] = ((1.0/3.0) * cvars_0.mmtx) + ((2.0/3.0) * cvars_2.mmtx) + ((2.0/3.0) * dt * cvars_1.mmtx)
     cvars_0.mmty[:] = ((1.0/3.0) * cvars_0.mmty) + ((2.0/3.0) * cvars_2.mmty) + ((2.0/3.0) * dt * cvars_1.mmty)
     cvars_0.enrg[:] = ((1.0/3.0) * cvars_0.enrg) + ((2.0/3.0) * cvars_2.enrg) + ((2.0/3.0) * dt * cvars_1.enrg)
 
 
-def calcRHS(cvars_ddt : ConsVars, cvars_now : ConsVars, dx : float, dy : float):
-    '''
-    Calculate RHS leaf task
-    '''
-    # Conservative to primitive
-    rho_coll = cvars_now.mass
-    u_coll   = cvars_now.mmtx / cvars_now.mass
-    v_coll   = cvars_now.mmty / cvars_now.mass
-    T_coll   = (cvars_now.enrg / cvars_now.mass - 0.5 * (u_coll * u_coll + v_coll * v_coll)) * (gamma - 1.0) / Rg
-    p_coll   = rho_coll * Rg * T_coll
 
-    # ASSEMBLE X - FLUXES
-    u   = interpX(u_coll)
-    v   = interpX(v_coll)
-    T   = interpX(T_coll)
-    p   = interpX(p_coll)
-    rho = p / (Rg * T)
-    H   = rho * (Rg * T * gamma / (gamma - 1.0) + 0.5 * (u * u + v * v)) 
-
-    flux_mass = -rho     * u
-    flux_mmtx = -rho * u * u - p
-    flux_mmty = -rho * v * u
-    flux_enrg = -H       * u
-
-    cvars_ddt.mass[:] = ddxStag(flux_mass, 1.0 / dx)
-    cvars_ddt.mmtx[:] = ddxStag(flux_mmtx, 1.0 / dx)
-    cvars_ddt.mmty[:] = ddxStag(flux_mmty, 1.0 / dx)
-    cvars_ddt.enrg[:] = ddxStag(flux_enrg, 1.0 / dx)
-
-    # ASSEMBLE Y - FLUXES
-    u   = interpY(u_coll)
-    v   = interpY(v_coll)
-    T   = interpY(T_coll)
-    p   = interpY(p_coll)
-    rho = p / (Rg * T)
-    H   = rho * (Rg * T * gamma / (gamma - 1.0) + 0.5 * (u * u + v * v)) 
-
-    flux_mass = -rho     * v
-    flux_mmtx = -rho * u * v
-    flux_mmty = -rho * v * v - p
-    flux_enrg = -H       * v
-
-    cvars_ddt.mass[:] += ddyStag(flux_mass, 1.0 / dy)
-    cvars_ddt.mmtx[:] += ddyStag(flux_mmtx, 1.0 / dy)
-    cvars_ddt.mmty[:] += ddyStag(flux_mmty, 1.0 / dy)
-    cvars_ddt.enrg[:] += ddyStag(flux_enrg, 1.0 / dy)
-
-
-
+@jit(nopython=True)
 def ddxStag(f, inv_dx : float):
     '''
     Calculate the derivative along x-dimension to backward position
     '''
     N = f.shape[0]
-    ip1 = np.arange(N)
-    ip2 = (ip1 + 1) % N
-    im1 = (ip1 - 1 + N) % N
-    im2 = (ip1 - 2 + N) % N
+    im1 = np.arange(N)
+    ip2 = (im1 + 2) % N
+    ip1 = (im1 + 1) % N
+    im2 = (im1 - 1 + N) % N
     return inv_dx * ( (9.0/8.0) * (f[ip1, :] - f[im1, :]) - (1.0/24.0) * (f[ip2, :] - f[im2, :]))
 
 
+@jit(nopython=True)
 def ddyStag(f, inv_dy : float):
     '''
     Calculate the derivative along y-dimension to backward position
     '''
     N = f.shape[1]
-    jp1 = np.arange(N)
-    jp2 = (jp1 + 1) % N
-    jm1 = (jp1 - 1 + N) % N
-    jm2 = (jp1 - 2 + N) % N
+    jm1 = np.arange(N)
+    jp2 = (jm1 + 2) % N
+    jp1 = (jm1 + 1) % N
+    jm2 = (jm1 - 1 + N) % N
     return inv_dy * ( (9.0/8.0) * (f[:, jp1] - f[:, jm1]) - (1.0/24.0) * (f[:, jp2] - f[:, jm2]))
 
-
-def interpX(f):
-    '''
-    Interpolate along x-dimension to the forward position
-    '''
-    N = f.shape[0]
-    im1 = np.arange(N)
-    im2 = (im1 - 1 + N) % N 
-    ip1 = (im1 + 1) % N
-    ip2 = (im1 + 2) % N
-    return (9.0 / 16.0) * (f[ip1, :] + f[im1, :]) - (1.0 / 16.0) * (f[ip2, :] + f[im2, :])
-
-
-
-def interpY(f):
-    '''
-    Interpolate along y-dimension to the forward position
-    '''
-    N = f.shape[1]
-    jm1 = np.arange(N)
-    jm2 = (jm1 - 1 + N) % N 
-    jp1 = (jm1 + 1) % N
-    jp2 = (jm1 + 2) % N
-    return (9.0 / 16.0) * (f[:, jp1] + f[:, jm1]) - (1.0 / 16.0) * (f[:, jp2] + f[:, jm2])
-
+@jit(nopython=True)
 def weno5ZInterp(fm2, fm1, f00, fp1, fp2):
     '''
     5th-order WENO5-Z interpolation scheme.
@@ -296,6 +417,8 @@ def weno5ZInterp(fm2, fm1, f00, fp1, fp2):
 
     return (beta0 * fs0 + beta1 * fs1 + beta2 * fs2) / (beta0 + beta1 + beta2)
 
+
+@jit(nopython=True)
 def weno5JSInterp(fm2, fm1, f00, fp1, fp2):
     '''
     5th-order WENO5-Z interpolation scheme.
